2f23c77ed3905e8d81cc4783d40b26e053229d06
mohssine <mohssine-d@hotmail.com>
2023-03-17 11:12:25 +0100

    SideBar

--- Program.cs
+++ Program.cs
@@ -10,8 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
-
          
-builder.Services.AddBlazorBootstrap(); // Add this line
+builder.Services.AddBlazorBootstrap(); 
 await builder.Build().RunAsync();
