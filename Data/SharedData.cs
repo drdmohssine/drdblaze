eb270fe41a2170d5da35248d6f8533070aebbff8
mohssine <mohssine-d@hotmail.com>
2023-05-27 18:56:46 +0100

    graph styling

--- Data/SharedData.cs
+++ Data/SharedData.cs
@@ -1,10 +1,14 @@
-﻿using static drdblaze.Data.SNData;
+﻿using VisNetwork.Blazor;
+using VisNetwork.Blazor.Models;
+using static drdblaze.Data.SNData;
 
 namespace drdblaze.Data
 {
     
     public class SharedData
     {
+        public event Action OnChange;
+
         public Project MainProject = new Project()
         {
 
@@ -13,6 +17,54 @@ namespace drdblaze.Data
             Packets = new List<Packet>(),
             NotesCollection = new List<NotesCollection>(),
         };
+
+        public Network network;
+        public NetworkData data;
+        public List<Node> nodes = new List<Node>();
+        public  List<Edge> edges = new List<Edge>();
+        public string layoutDirection = "LR";
+        public string canvasColor = "#e8c6c8";
+        public string nodeColor = "#D2E5FF";
+        public string fontColor = "#000000";
+
+        public NetworkOptions EditorConstructionOptions(Network network)
+        {
+
+            return new NetworkOptions
+            {
+                Layout = new LayoutOptions()
+                {
+                    Hierarchical = new HierarchicalOptions()
+                    {
+                        Direction = layoutDirection,
+                        SortMethod = "directed",
+                        ShakeTowards = "leaves",
+
+                        LevelSeparation = 300
+                    }
+                },
+
+                Physics = new PhysicsOptions()
+                {
+                    //Enabled = false
+                    BarnesHut = new BarnesHutOption()
+                    {
+
+                        AvoidOverlap = 1,
+                        Damping = 1,
+                    }
+                }
+
+            };
+        }
+
+
+
+
+
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+
     }
 
 
